f2405132345fb6323233ec87b9e8e0b9246152e1
Antti L <a.lipsanen@gmail.com>
2018-05-05 15:39:23 +0300

    Refactor editor and grid dependencies

--- Assets/Scripts/EditorCubeGridSnap.cs
+++ Assets/Scripts/EditorCubeGridSnap.cs
@@ -2,32 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 [ExecuteInEditMode]
+[RequireComponent(typeof(Waypoint))]
 public class EditorCubeGridSnap: MonoBehaviour {
 
 	//Range restricts values in inspector, SizeOfGrid related to the size of the cube that needs to be snapped
-	[SerializeField] [Range(1f, 20f)] float sizeOfGrid = 10f;
+
 
 	private TextMesh labelMesh;
 	private string cubeCoordinates;
+	private Vector3 gridPosition;
+	private Waypoint cubeWaypoint;
+	private int sizeOfGrid;
+
 
 	//Awake is called when played
 	//Update is called when editing
 	// Use this for initialization
 	void Awake () {
 		labelMesh = GetComponentInChildren<TextMesh>();
+		cubeWaypoint = GetComponent<Waypoint>();
+		print(cubeWaypoint);
+		sizeOfGrid = cubeWaypoint.GetGridSize();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 snapPos;
-		snapPos.x = Mathf.RoundToInt(transform.position.x / sizeOfGrid) * sizeOfGrid;
-		snapPos.z = Mathf.RoundToInt(transform.position.z / sizeOfGrid) * sizeOfGrid;
+		SnapCubeToGrid();
+		UpdateLabel();
+		}
 
-		transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+	private void SnapCubeToGrid() {
+		gridPosition.x = cubeWaypoint.GetGridPos().x;
+		gridPosition.z = cubeWaypoint.GetGridPos().y;
+		transform.position = new Vector3(gridPosition.x, 0f, gridPosition.z);
+		}
 
-		cubeCoordinates = snapPos.x / sizeOfGrid + "," + snapPos.z / sizeOfGrid;
+	private void UpdateLabel() {
+		cubeCoordinates = gridPosition.x / sizeOfGrid + "," + gridPosition.z / sizeOfGrid;
 		labelMesh.text = cubeCoordinates;
 		gameObject.name = "Cube " + cubeCoordinates;
 		}
-}
+	}
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour {
 
+	const int sizeOfGrid = 10;
+	//waypoint needs to know it's own position -> calculate position on grid in waypoint script 
+
+	Vector2Int gridPos;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,16 @@ public class Waypoint : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		}
+
+	public Vector2 GetGridPos() {
+		return new Vector2Int(
+		Mathf.RoundToInt(transform.position.x / sizeOfGrid) * sizeOfGrid,
+		Mathf.RoundToInt(transform.position.z / sizeOfGrid) * sizeOfGrid);
+		}
+
+	public int GetGridSize() {
+		return sizeOfGrid;
+		}
+
 }
