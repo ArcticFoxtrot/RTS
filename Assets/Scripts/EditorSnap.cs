1523d32b98a499e9ebf43359527e7db34668e06f
Antti L <a.lipsanen@gmail.com>
2018-05-05 09:39:06 +0300

    Added editor snapping to grid

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour {
+
+	//Range restricts values in inspector, SizeOfGrid related to the size of the cube that needs to be snapped
+	[SerializeField] [Range(1f, 20f)] float sizeOfGrid = 10f;
+
+	//Awake is called when played
+	//Update is called when editing
+	// Use this for initialization
+	void Awake () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 snapPos;
+		snapPos.x = Mathf.RoundToInt(transform.position.x / sizeOfGrid) * sizeOfGrid;
+		snapPos.z = Mathf.RoundToInt(transform.position.z / sizeOfGrid) * sizeOfGrid;
+
+		transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+		}
+}
