a73237dbdf62ab1704f82f5be79f5ea6dae727e7
Antti L <a.lipsanen@gmail.com>
2018-07-01 20:19:41 +0300

    prework on spawning

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	[SerializeField] float secondsBetweenSpawns = 2f;
+	[SerializeField] EnemyMovement enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+		//start a coroutine
+	}
+	
+	// Update is called once per frame
+
+
+	IEnumerator SpawnEnemy() {
+		// forever
+			// spawn
+			// wait
+		}
+}
